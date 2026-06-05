3511f6b5177db1ca5b26340c44c2d04ad148479c
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2026-06-05 16:42:12 +0530

    fix timeout error

--- controller/mailer.js
+++ controller/mailer.js
@@ -3,7 +3,9 @@ const nodemailer=require("nodemailer");
 const mailer=function(email,status,callback){
 
     const transport=nodemailer.createTransport({
-        service:"gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth:{
             user:process.env.USER_EMAIL,
             pass:process.env.EMAIL_PASSWORD,
@@ -20,10 +22,19 @@ const mailer=function(email,status,callback){
         html:`Hello ${email}, This is simply a registration link which is given below, you must need to click on the 
         below link to register yourself.
         <br><br>
-        <form action='http://localhost:3000/${endPoint}' method='post'>
-           <input type='hidden' name='email' id='email' value='${email}'>
-           <button>Click To Register</button>
-        </form>
+        <a
+        href="http://localhost:3000/student/studentRegistration?email=${encodeURIComponent(email)}"
+        style="
+            background:#2563eb;
+            color:white;
+            padding:10px 20px;
+            text-decoration:none;
+            border-radius:5px;
+            display:inline-block;
+        "
+        >
+        Click To Register
+        </a>
         `
     }
 
